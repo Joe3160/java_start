9c4bc59da30e8c5730fe0a956707016bb43330ac
Joe3160 <694995669@qq.com>
2017-12-11 14:59:10 +0800

    运算符

--- src/day12/Operator.java
+++ src/day12/Operator.java
@@ -11,6 +11,13 @@ public class Operator {
         System.out.println("i1*i2="+(i1*i2));
         System.out.println("i1/i2="+(i1/(double)i2));
 
+        System.out.println("---------------------");
+
+        //声名double变量
+        double d1=8.0;
+        double d2=2.5;
+        System.out.println("d1="+d1+"\nd2="+d2);
+
 
     }
 }
