8bab3f977095666188bf198ac05cc3f27832c5f0
Joe3160 <694995669@qq.com>
2017-12-11 17:29:11 +0800

    赋值运算符

--- src/day13/Demo.java
+++ src/day13/Demo.java
@@ -5,17 +5,42 @@ package day13;
  */
 public class Demo {
     public static  void main(String[] args){
-        int i=1;
-        i+=5;
-        System.out.println(i);//6
-        i-=2;
-        System.out.println(i);//4
-        i*=2;
-        System.out.println(i);//8
-        i/=3;
-        System.out.println(i);//2
-        i%=2;
-        System.out.println(i);//0
+        //赋值运算符
+        int i2=1;
+        i2+=5;
+        System.out.println(i2);//6
+        i2-=2;
+        System.out.println(i2);//4
+        i2*=2;
+        System.out.println(i2);//8
+        i2/=3;
+        System.out.println(i2);//2
+        i2%=2;
+        System.out.println(i2);//0
+
+        System.out.println("------------------自增、自减--------------------");
+        //自增
+        System.out.println("--------自增-------------");
+        int i3 =20;
+        System.out.println("i3="+i3);//20
+        System.out.println("++i3="+(++i3));//21
+        System.out.println("i3="+i3);//21
+        System.out.println("i3++="+(i3++));//21
+        System.out.println("i3="+i3);//22
+
+        //自减
+        System.out.println("--------自减-------------");
+        int i4=40;
+        System.out.println("i4="+i4);//40
+        System.out.println("--i4="+(--i4));//39
+        System.out.println("i4="+i4);//39
+        System.out.println("i4--="+(i4--));//39
+        System.out.println("i4="+i4);//38
+
+
+
+
+
 
 
     }
