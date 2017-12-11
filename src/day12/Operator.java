3ebb4f5a3d14a507765a745df91c49e486f10dcd
Joe3160 <694995669@qq.com>
2017-12-11 14:42:22 +0800

    day12

---
+++ src/day12/Operator.java
@@ -0,0 +1,16 @@
+package day12;
+
+public class Operator {
+    public static void main(String[] args) {
+        //声名变量
+        int i1=7;
+        int i2=2;
+        System.out.println("i1="+i1+"\ni2="+i2);
+        System.out.println("i1+i2="+(i1+i2));
+        System.out.println("i1-i2="+(i1-i2));
+        System.out.println("i1*i2="+(i1*i2));
+        System.out.println("i1/i2="+(i1/(double)i2));
+
+
+    }
+}
