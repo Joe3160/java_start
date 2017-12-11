eaf0951f7ff9f2348e4244452b82b14e33a6837d
Joe3160 <694995669@qq.com>
2017-12-11 17:05:44 +0800

    赋值运算符

---
+++ src/day13/Demo.java
@@ -0,0 +1,22 @@
+package day13;
+
+/***
+ * 赋值运算符
+ */
+public class Demo {
+    public static  void main(String[] args){
+        int i=1;
+        i+=5;
+        System.out.println(i);//6
+        i-=2;
+        System.out.println(i);//4
+        i*=2;
+        System.out.println(i);//8
+        i/=3;
+        System.out.println(i);//2
+        i%=2;
+        System.out.println(i);//0
+
+
+    }
+}
