893e88f1eb7763763b2f8a5d94199a8c94c95707
Joe3160 <694995669@qq.com>
2017-12-13 11:01:47 +0800

    do while

--- src/day16/Demo.java
+++ src/day16/Demo.java
@@ -5,12 +5,21 @@ package day16;
  */
 public class Demo {
     public static void main(String[] args){
+        //while循环
         int i=0;
         while (i <= 10) {
             System.out.println(i);
             i++;
         }
 
-        
+        System.out.println("---------------");
+        //do……while循环
+        int j=20;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j<=10);
+
+
     }
 }
