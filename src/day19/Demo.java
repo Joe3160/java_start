20b87359db480ee208d2988f6dfb6f5d2db79541
Joe3160 <694995669@qq.com>
2017-12-13 18:37:22 +0800

    day19

---
+++ src/day19/Demo.java
@@ -0,0 +1,15 @@
+package day19;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args){
+        int[] arr1=new int[15];
+        System.out.println(arr1.length);
+        int user;
+        double sum=0,svg=0;
+        double[] temp=new double[10];
+        //创建一个scanner对象，用它来获得用户的输入
+        Scanner sc=new Scanner(System.in);
+    }
+}
