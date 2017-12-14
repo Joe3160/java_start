e3a8a6d9a03b0dc6dec2bb4e4d1a55bcf5b4490b
Joe3160 <694995669@qq.com>
2017-12-14 12:27:49 +0800

    多维数组

---
+++ src/day20/Demo.java
@@ -0,0 +1,14 @@
+package day20;
+
+import common.helper;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //声明二维数组
+        int[][] arr1 = new int[2][3];
+        int[][] arr2 = {{1, 3, 5}, {2, 4, 6}};
+
+
+    }
+}
