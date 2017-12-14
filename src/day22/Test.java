cdf4733cc9a166e1e0a419c7b6770632c286cbd8
Joe3160 <694995669@qq.com>
2017-12-14 18:22:49 +0800

    方法与主方法

---
+++ src/day22/Test.java
@@ -0,0 +1,12 @@
+package day22;
+
+import common.helper;
+
+public class Test {
+    public static void main(String[] args){
+        User user=new User();
+        String hi=user.hi();
+        helper.dump(hi);
+
+    }
+}
---
+++ src/day22/User.java
@@ -0,0 +1,20 @@
+package day22;
+
+import common.helper;
+
+public class User {
+    String name;
+    String gender;
+    int age;
+
+
+    public String hi() {
+        return "Hello";
+    }
+
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+
+}
