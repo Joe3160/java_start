31431da9adb0d6d5f3e077cb6e7fa5ca575afb0a
Joe3160 <694995669@qq.com>
2018-01-02 11:47:43 +0800

    test

---
+++ src/day31/User.java
@@ -0,0 +1,27 @@
+package day31;
+
+public class User {
+    public String getName() {
+        return name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getMobile() {
+        return mobile;
+    }
+    
+    private String name;
+    private int id;
+    private String email;
+    private String mobile;
+    
+    
+    
+}
